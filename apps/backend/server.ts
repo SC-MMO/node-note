8bbe165cce17e933658323bc6a427df6a23f4806
SC-MMO <moritz.mummert@scaltel.de>
2026-03-11 09:50:49 +0100

    Small improvements

--- apps/backend/server.ts
+++ apps/backend/server.ts
@@ -2,20 +2,16 @@ import express from "express";
 import { router } from "./api";
 const app = express();
 
-// cors
 const cors = require("cors");
-const corsOptions = { origin: ["http://10.12.19.19:5173"], credentials: true };
+const corsOptions = { credentials: true };
 app.use(cors(corsOptions));
 
-// json parsing for axios?
 app.use(express.json());
 
-//startup
+app.use("/api", router);
+
 const port: number = +(process.env.PORT || 3000);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
-
-// routes
-app.use("/api", router);
